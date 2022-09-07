4bb429df7bdc5ae5a6299a354c136cc7a65174f7
Anselmo Henrique <100934388+Anselmo5@users.noreply.github.com>
2022-09-07 09:25:59 -0300

    Add files via upload

---
+++ lex.js
@@ -0,0 +1,16 @@
+var cor = document.getElementById("corinput")
+const fundo = document.getElementById("fundo")
+
+function trocacor(){
+    let c = cor.value 
+    fundo.style.backgroundColor = c
+    console.log(cor);
+}
+
+//if (corblack){
+//} 
+
+//else (corbranca){
+
+//}
+
